2041c8d8695b5d1f1e0968f5b8ff27e0949adbb6
Luis Enrique <luisebt995@gmail.com>
2024-02-19 18:23:32 +0100

    Logic for NavigationGraph and minor changes.

--- app/src/main/java/com/example/fragmentsproyect/ResultFragment.kt
+++ app/src/main/java/com/example/fragmentsproyect/ResultFragment.kt
@@ -1,19 +1,38 @@
 package com.example.fragmentsproyect
 
+import android.media.Image
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.navArgs
 
 class ResultFragment : Fragment() {
+    val args: ResultFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+
+        val root = inflater.inflate(R.layout.fragment_result, container, false)
+        val imgResult = root.findViewById<ImageView>(R.id.imgResult)
+
+        if (savedInstanceState == null) {
+            if (args.warriorType == 1){
+                Log.i("ResultFragment", "Conan Warrior")
+                imgResult.setImageResource(R.drawable.conan)
+            }
+            if (args.warriorType == 2){
+                Log.i("ResultFragment", "Conan Warrior")
+                imgResult.setImageResource(R.drawable.xena)
+            }
+        }
+
+        return root
     }
 
 }
--- app/src/main/java/com/example/fragmentsproyect/SelectionFragment.kt
+++ app/src/main/java/com/example/fragmentsproyect/SelectionFragment.kt
@@ -1,10 +1,14 @@
 package com.example.fragmentsproyect
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 
 class SelectionFragment : Fragment() {
 
@@ -12,8 +16,37 @@ class SelectionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_selection, container, false)
+
+        val root = inflater.inflate(R.layout.fragment_selection, container, false)
+        val imgConan = root.findViewById<ImageView>(R.id.imgConan)
+        val imgXena = root.findViewById<ImageView>(R.id.imgXena)
+
+        imgConan.setOnClickListener{
+            val action = SelectionFragmentDirections.actionSelectionFragmentToResultFragment(1)
+            showAlertDialog(action,"Conan")
+        }
+
+        imgXena.setOnClickListener{
+            val action = SelectionFragmentDirections.actionSelectionFragmentToResultFragment(2)
+            showAlertDialog(action,"Xena")
+        }
+
+        return root
+    }
+
+    private fun showAlertDialog(action: NavDirections, warrior: String) {
+        val builder = AlertDialog.Builder(requireContext())
+            .setTitle("SELECCION DEL GUERRERO")
+            .setMessage("¿Seguro que quieres seleccionar a $warrior ?")
+            .setCancelable(false) // Evita que el usuario cierre el diálogo haciendo clic fuera de él o presionando el botón de retroceso
+            .setPositiveButton("Aceptar") { dialog, _ -> // Acción cuando se hace clic en el botón "Aceptar"
+                findNavController().navigate(action)
+                dialog.dismiss() // Cierra el diálogo
+            }
+                .setNegativeButton("Cancelar") { dialog, _ ->  // Acción cuando se hace clic en el botón "Cancelar"
+                dialog.dismiss() // Cierra el diálogo
+            }
+                    .show()
     }
 
 }
